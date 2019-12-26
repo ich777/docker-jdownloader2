7f10ddf0dc8a66c539dbba0cf9ada6c007a3a68e
ich777 <ich777@noreply.example.org>
2019-12-26 23:12:07 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -50,7 +50,7 @@ echo "---Checking for old logfiles---"
 find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
 find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
 echo "---Checking for old display lock files---"
-find /tmp -name ".X99*" -exec rm -f {} \;
+find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
 
 echo "---Resolution check---"
 if [ -z "${CUSTOM_RES_W} ]; then
@@ -107,13 +107,13 @@ chmod -R 777 ${DATA_DIR}
 
 echo "---Starting Xvfb server---"
 screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
-sleep 5
+sleep 2
 echo "---Starting x11vnc server---"
 screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
-sleep 5
+sleep 2
 echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
-sleep 5
+sleep 2
 
 echo "---Starting jDownloader2---"
 export DISPLAY=:99
