e02e209c11303214797e62c4d16117dacd141aee
ich777 <ich777@noreply.example.org>
2020-02-25 12:42:03 +0100

    „scripts/start.sh“ hinzufügen

---
+++ scripts/start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "---Checking if UID: ${UID} matches user---"
+usermod -u ${UID} ${USER}
+echo "---Checking if GID: ${GID} matches user---"
+usermod -g ${GID} ${USER}
+echo "---Setting umask to ${UMASK}---"
+umask ${UMASK}
+
+echo "---Checking for optional scripts---"
+if [ -f /opt/scripts/user.sh ]; then
+	echo "---Found optional script, executing---"
+    chmod +x /opt/scripts/user.sh
+    /opt/scripts/user.sh
+else
+	echo "---No optional script found, continuing---"
+fi
+
+echo "---Starting...---"
+chown ${UID}:${GID} /opt/scripts
+chown ${UID}:${GID} /opt/scripts/start-server.sh
+chown -R ${UID}:${GID} /mnt
+chmod -R 770 /mnt
+su ${USER} -c "/opt/scripts/start-server.sh"
