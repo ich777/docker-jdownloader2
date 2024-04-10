9f6540f73514dd9f36008c9a4321fa1cd723a7c5
Christoph Hummer <christophhummer@gmail.com>
2024-04-10 19:56:10 +0200

    scripts/start-connected-containers.sh hinzugefügt

---
+++ scripts/start-connected-containers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Check if variable is set correctly
+if [ -z "${CONNECTED_CONTAINERS%%:*}" ] || [ -z "${CONNECTED_CONTAINERS#*:}" ] || [[ "${CONNECTED_CONTAINERS}" != *:* ]]; then
+  echo "---The variable CONNECTED_CONTAINERS is not set properly!---"
+  echo "---Please set it like: 127.0.0.1:27286---"
+  exit 1
+fi
+
+# Wait 5 seconds to start the connection
+sleep 5
+echo "---Starting connected containers watchdog on ${CONNECTED_CONTAINERS}---"
+nc ${CONNECTED_CONTAINERS%%:*} ${CONNECTED_CONTAINERS#*:}
+EXIT_STATUS=$?
+
+# Determin on exit status if connection was successfull or if container should be restarted
+if [ "${EXIT_STATUS}" != 0 ]; then
+  echo "---Couldn't connect to: ${CONNECTED_CONTAINERS%%:*} on port: ${CONNECTED_CONTAINERS#*:}"
+  exit 1
+else
+  echo "---Connection to connected container: ${CONNECTED_CONTAINERS} lost, restarting in 5 seconds...---"
+  sleep 5
+  kill -SIGTERM $(pidof java)
+fi
